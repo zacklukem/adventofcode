b780c8c8418b8c75632600366456a837962957f8
Zachary Mayhew <mayhew.zachary2003@gmail.com>
2024-12-02 08:19:32 -0500

    2024 Day 1

--- 2024/lib/day1.ex
+++ 2024/lib/day1.ex
@@ -1,7 +1,42 @@
 defmodule Day1a do
   def run do
     File.read!(".input.txt")
+    |> String.split("\n", trim: true)
+    |> Enum.map(fn line ->
+      line
+      |> String.split(" ", trim: true)
+      |> Enum.map(&String.to_integer/1)
+    end)
+    |> List.zip()
+    |> Enum.map(&Tuple.to_list/1)
+    |> Enum.map(&Enum.sort/1)
+    |> List.zip()
+    |> Enum.map(fn {a, b} -> abs(a - b) end)
+    |> Enum.sum()
+  end
+end
 
-    raise "Not implemented"
+defmodule Day1b do
+  def run do
+    [left, right] =
+      File.read!(".input.txt")
+      |> String.split("\n", trim: true)
+      |> Enum.map(fn line ->
+        line
+        |> String.split(" ", trim: true)
+        |> Enum.map(&String.to_integer/1)
+      end)
+      |> List.zip()
+      |> Enum.map(&Tuple.to_list/1)
+
+    right_freq =
+      right
+      |> Enum.reduce(Map.new(), fn x, acc ->
+        acc |> Map.update(x, 1, &(&1 + 1))
+      end)
+
+    left
+    |> Enum.map(fn x -> x * Map.get(right_freq, x, 0) end)
+    |> Enum.sum()
   end
 end
