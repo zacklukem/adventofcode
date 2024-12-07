1ee78391c6929fda18643a0652b6ded6646f256a
Zachary Mayhew <mayhew.zachary2003@gmail.com>
2024-12-07 07:52:57 -0500

    2024 Day 7 Faster

--- 2024/lib/day7.ex
+++ 2024/lib/day7.ex
@@ -23,12 +23,26 @@ defmodule Day7a do
 end
 
 defmodule Day7b do
-  def concat(a, b) do
-    String.to_integer("#{a}#{b}")
+  def concat(a, b, bases \\ [10, 100, 1000, 10000])
+
+  def concat(a, b, [base | _]) when b < base do
+    a * base + b
+  end
+
+  def concat(a, b, [_ | rest]) do
+    concat(a, b, rest)
+  end
+
+  def concat(a, b, []) do
+    a * :math.pow(10, :math.ceil(:math.log10(b))) + b
   end
 
   def is_possible?(num, [value]) do
     value == num
+  end
+
+  def is_possible?(num, [a | _]) when a > num do
+    false
   end
 
   def is_possible?(num, [a | [b | rest]]) do
@@ -45,8 +59,10 @@ defmodule Day7b do
       {String.to_integer(num),
        raw_operands |> String.split(" ") |> Enum.map(&String.to_integer/1)}
     end)
-    |> Enum.filter(fn {num, operands} -> is_possible?(num, operands) end)
-    |> Enum.map(fn {num, _} -> num end)
+    |> Task.async_stream(fn {num, operands} -> {num, is_possible?(num, operands)} end)
+    |> Stream.map(fn {:ok, val} -> val end)
+    |> Stream.filter(fn {_, possible} -> possible end)
+    |> Stream.map(fn {num, _} -> num end)
     |> Enum.sum()
   end
 end
